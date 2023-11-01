3d6e5d18e2277a148aedcd0a53775cd1d55e3509
changwoolab <cwyoo01@naver.com>
2023-11-01 23:19:23 +0900

    rn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+use std::process::Command;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide a command");
+        return;
+    }
+
+    if args.len() > 2 {
+        println!("Please provide only one command");
+        return;
+    }
+
+    let command = &args[1];
+    match command.as_str() {
+        "--rn-debugger" => {
+            Command::new("killall")
+                .arg("React Native Debugger")
+                .spawn()
+                .expect("failed to execute process");
+
+            thread::sleep(time::Duration::from_millis(1500));
+
+            Command::new("open")
+                .arg("-a")
+                .arg("React Native Debugger")
+                .spawn()
+                .expect("failed to execute process");
+
+            return;
+        },
+        _ => {}
+    }
 }
